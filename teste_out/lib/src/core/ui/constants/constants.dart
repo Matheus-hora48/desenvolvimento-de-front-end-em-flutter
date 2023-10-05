99a5a336f5e909705f954e3612e367258d51cff8
Matheus Hora <matheus11hora@gmail.com>
2023-10-04 23:59:00 -0300

    feat: estruturando o projeto

---
+++ teste_out/lib/src/core/ui/constants/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+sealed class ColorsConstants {
+  static const blue = Color(0xFF4E97FE);
+  static const bg = Color(0xFFF9F9FF);
+  static const purple = Color(0xFF7A5CF0);
+  static const black = Color(0xFF1A1C1E);
+  static const grey = Color(0xFF46474A);
+  static const lightGrey = Color(0xFF909094);
+  static const borderColor = Color(0xFFC7C6CA);
+  static const lineColor = Color(0xFFE3E2E6);
+  static const appBarTextColor = Color(0xff001B3D);
+  static const errorColor = Color(0xffBA1A1A);
+}
+
+sealed class FontsConstants {
+  static const fontFamily = 'Open Sans';
+}
